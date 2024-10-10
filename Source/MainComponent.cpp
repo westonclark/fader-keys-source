fa1330bb79bf5ccee0602b2829a8b302ab82bfdd
Weston Clark <westoclark@gmail.com>
2024-10-09 21:48:00 -0700

    Inital scaffolding, keys triggering midi messages on keyboard

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,23 +1,89 @@
 #include "MainComponent.h"
+#include <map>
+#include <cctype> // For std::tolower
 
 //==============================================================================
 MainComponent::MainComponent()
+    : keyboardComponent(keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard)
 {
     // Make sure you set the size of the component after
     // you add any child components.
     setSize(800, 600);
 
-    // Some platforms require permissions to open input channels so request that here
-    if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio) && !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
+    // Prevent the keyboard component from stealing focus
+    keyboardComponent.setWantsKeyboardFocus(false);
+
+    // Add the keyboard component to the main component
+    addAndMakeVisible(keyboardComponent);
+
+    // Add KeyListener to capture keystrokes
+    setWantsKeyboardFocus(true);
+    addKeyListener(this);
+
+    // Setup MIDI devices
+    setupMidiDevices();
+
+    // Add MIDI output selector
+    addAndMakeVisible(midiOutputSelector);
+    midiOutputSelector.onChange = [this]
     {
-        juce::RuntimePermissions::request(juce::RuntimePermissions::recordAudio,
-                                          [&](bool granted)
-                                          { setAudioChannels(granted ? 2 : 0, 2); });
+        closeMidiDevices();
+        setupMidiDevices();
+    };
+    midiOutputLabel.setText("MIDI Output:", juce::dontSendNotification);
+    addAndMakeVisible(midiOutputLabel);
+
+    // Add MIDI input selector
+    addAndMakeVisible(midiInputSelector);
+    midiInputSelector.onChange = [this]
+    {
+        closeMidiDevices();
+        setupMidiDevices();
+    };
+    midiInputLabel.setText("MIDI Input:", juce::dontSendNotification);
+    addAndMakeVisible(midiInputLabel);
+
+    // Populate MIDI devices
+    auto midiOutputs = juce::MidiOutput::getAvailableDevices();
+    for (auto &output : midiOutputs)
+    {
+        midiOutputSelector.addItem(output.name, output.identifier.hashCode());
+    }
+
+    auto midiInputs = juce::MidiInput::getAvailableDevices();
+    for (auto &input : midiInputs)
+    {
+        midiInputSelector.addItem(input.name, input.identifier.hashCode());
+    }
+
+    // Register keyboard state listener
+    keyboardState.addListener(this);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio) &&
+        !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
+    {
+        juce::RuntimePermissions::request(
+            juce::RuntimePermissions::recordAudio,
+            [&](bool granted)
+            { setAudioChannels(granted ? 2 : 0, 2); });
     }
     else
     {
         // Specify the number of input and output channels that we want to open
-        setAudioChannels(2, 2);
+        setAudioChannels(0, 2);
+    }
+
+    // Remove grabKeyboardFocus() from constructor
+}
+
+// Overridden to handle visibility change
+void MainComponent::visibilityChanged()
+{
+    if (isShowing() && !hasFocusBeenSet)
+    {
+        grabKeyboardFocus();
+        hasFocusBeenSet = true;
     }
 }
 
@@ -25,51 +91,222 @@ MainComponent::~MainComponent()
 {
     // This shuts down the audio device and clears the audio source.
     shutdownAudio();
+
+    // Close MIDI devices and listeners
+    closeMidiDevices(); // Resets the unique_ptrs
+
+    keyboardState.removeListener(this);
+    removeKeyListener(this);
 }
 
 //==============================================================================
-void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay(int samplesPerBlockExpected,
+                                  double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+    // Not used since we're not processing audio
 }
 
-void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo &bufferToFill)
+void MainComponent::getNextAudioBlock(
+    const juce::AudioSourceChannelInfo &bufferToFill)
 {
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
+    // Since we're not producing any audio, clear the buffer
     bufferToFill.clearActiveBufferRegion();
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
+    // Not used since we're not processing audio
 }
 
 //==============================================================================
 void MainComponent::paint(juce::Graphics &g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-
-    // You can add your drawing code here!
+    // Fill the background
+    g.fillAll(
+        getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    // Position the keyboard component
+    keyboardComponent.setBounds(10, getHeight() - 110, getWidth() - 20, 100);
+
+    // Position the MIDI output selector
+    midiOutputLabel.setBounds(10, 10, 80, 24);
+    midiOutputSelector.setBounds(100, 10, getWidth() - 110, 24);
+
+    // Position the MIDI input selector
+    midiInputLabel.setBounds(10, 40, 80, 24);
+    midiInputSelector.setBounds(100, 40, getWidth() - 110, 24);
+}
+
+// KeyListener callbacks
+bool MainComponent::keyPressed(const juce::KeyPress &key, juce::Component *originatingComponent)
+{
+    DBG("Key Pressed: " << key.getTextDescription());
+    // Map keys to MIDI notes starting from middle C
+    int baseMidiNote = 60; // Middle C
+
+    // Define a mapping from keys to MIDI note offsets
+    std::map<char, int> keyToNoteOffset = {
+        {'a', 0},  // C4
+        {'s', 2},  // D4
+        {'d', 4},  // E4
+        {'f', 5},  // F4
+        {'g', 7},  // G4
+        {'h', 9},  // A4
+        {'j', 11}, // B4
+        {'k', 12}, // C5
+        {'l', 14}, // D5
+        {';', 16}, // E5
+        {'\'', 17} // F5
+    };
+
+    char keyChar = key.getTextCharacter();
+    keyChar = std::tolower(keyChar);
+
+    if (keyToNoteOffset.find(keyChar) != keyToNoteOffset.end())
+    {
+        int midiNote = baseMidiNote + keyToNoteOffset[keyChar];
+        float velocity = 1.0f;
+
+        juce::MidiMessage noteOn = juce::MidiMessage::noteOn(1, midiNote, velocity);
+        noteOn.setTimeStamp(juce::Time::getMillisecondCounterHiRes() * 0.001);
+
+        // Send MIDI message
+        if (midiOutput != nullptr)
+            midiOutput->sendMessageNow(noteOn);
+
+        // Update keyboard state
+        keyboardState.noteOn(1, midiNote, velocity);
+
+        return true; // Key event was handled
+    }
+
+    return false; // Key event was not handled
+}
+
+bool MainComponent::keyStateChanged(bool isKeyDown, juce::Component *originatingComponent)
+{
+    DBG("Key State Changed");
+
+    // Handle key releases to send Note Off messages
+    // Since JUCE doesn't call keyPressed on key release, we need to handle key state changes
+
+    // Map keys to MIDI notes starting from middle C
+    int baseMidiNote = 60; // Middle C
+
+    // Define a mapping from keys to MIDI note offsets
+    std::map<char, int> keyToNoteOffset = {
+        {'a', 0},  // C4
+        {'s', 2},  // D4
+        {'d', 4},  // E4
+        {'f', 5},  // F4
+        {'g', 7},  // G4
+        {'h', 9},  // A4
+        {'j', 11}, // B4
+        {'k', 12}, // C5
+        {'l', 14}, // D5
+        {';', 16}, // E5
+        {'\'', 17} // F5
+    };
+
+    // Iterate over all mapped keys
+    for (auto &keyMapping : keyToNoteOffset)
+    {
+        char keyChar = keyMapping.first;
+        int midiNote = baseMidiNote + keyMapping.second;
+
+        if (!juce::KeyPress::isKeyCurrentlyDown(keyChar))
+        {
+            // Key is released
+            // Send Note Off message
+            float velocity = 0.0f; // Velocity for Note Off
+            juce::MidiMessage noteOff = juce::MidiMessage::noteOff(1, midiNote, velocity);
+            noteOff.setTimeStamp(juce::Time::getMillisecondCounterHiRes() * 0.001);
+
+            // Send MIDI message
+            if (midiOutput != nullptr)
+                midiOutput->sendMessageNow(noteOff);
+
+            // Update keyboard state
+            keyboardState.noteOff(1, midiNote, velocity);
+        }
+    }
+
+    return false;
+}
+
+// MidiInputCallback implementation
+void MainComponent::handleIncomingMidiMessage(juce::MidiInput *source, const juce::MidiMessage &message)
+{
+    // Handle incoming MIDI messages
+    // For example, update the keyboard state
+    keyboardState.processNextMidiEvent(message);
+}
+
+// MidiKeyboardStateListener implementation
+void MainComponent::handleNoteOn(juce::MidiKeyboardState *source, int midiChannel, int midiNoteNumber, float velocity)
+{
+    // This is called when a note is pressed on the virtual keyboard
+    // You can handle this event if needed
+}
+
+void MainComponent::handleNoteOff(juce::MidiKeyboardState *source, int midiChannel, int midiNoteNumber, float velocity)
+{
+    // This is called when a note is released on the virtual keyboard
+    // You can handle this event if needed
+}
+
+// Setup MIDI devices
+void MainComponent::setupMidiDevices()
+{
+    // Open selected MIDI output device
+    if (midiOutput == nullptr)
+    {
+        auto selectedOutputId = midiOutputSelector.getSelectedId();
+        auto midiOutputs = juce::MidiOutput::getAvailableDevices();
+
+        for (auto &output : midiOutputs)
+        {
+            if (output.identifier.hashCode() == selectedOutputId)
+            {
+                midiOutput = juce::MidiOutput::openDevice(output.identifier);
+                break;
+            }
+        }
+    }
+
+    // Open selected MIDI input device
+    if (midiInput == nullptr)
+    {
+        auto selectedInputId = midiInputSelector.getSelectedId();
+        auto midiInputs = juce::MidiInput::getAvailableDevices();
+
+        for (auto &input : midiInputs)
+        {
+            if (input.identifier.hashCode() == selectedInputId)
+            {
+                midiInput = juce::MidiInput::openDevice(input.identifier, this);
+                if (midiInput != nullptr)
+                    midiInput->start();
+                break;
+            }
+        }
+    }
+}
+
+// Close MIDI devices
+void MainComponent::closeMidiDevices()
+{
+    if (midiOutput != nullptr)
+    {
+        midiOutput.reset(); // Resets the unique_ptr, closing the device
+    }
+
+    if (midiInput != nullptr)
+    {
+        midiInput->stop();
+        midiInput.reset(); // Resets the unique_ptr, closing the device
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,10 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent : public juce::AudioAppComponent
+class MainComponent : public juce::AudioAppComponent,
+                      public juce::KeyListener,
+                      public juce::MidiInputCallback,
+                      public juce::MidiKeyboardStateListener
 {
 public:
     //==============================================================================
@@ -23,9 +26,42 @@ public:
     void paint(juce::Graphics &g) override;
     void resized() override;
 
+    // KeyListener callbacks
+    bool keyPressed(const juce::KeyPress &key, juce::Component *originatingComponent) override;
+    bool keyStateChanged(bool isKeyDown, juce::Component *originatingComponent) override;
+
+    // MidiInputCallback
+    void handleIncomingMidiMessage(juce::MidiInput *source, const juce::MidiMessage &message) override;
+
+    // MidiKeyboardStateListener
+    void handleNoteOn(juce::MidiKeyboardState *source, int midiChannel, int midiNoteNumber, float velocity) override;
+    void handleNoteOff(juce::MidiKeyboardState *source, int midiChannel, int midiNoteNumber, float velocity) override;
+
+    // Overridden to handle visibility change
+    void visibilityChanged() override;
+
 private:
     //==============================================================================
     // Your private member variables go here...
+
+    juce::MidiKeyboardState keyboardState;
+    juce::MidiKeyboardComponent keyboardComponent;
+
+    // MIDI devices
+    std::unique_ptr<juce::MidiOutput> midiOutput;
+    juce::ComboBox midiOutputSelector;
+
+    std::unique_ptr<juce::MidiInput> midiInput;
+    juce::ComboBox midiInputSelector;
+
+    juce::Label midiOutputLabel;
+    juce::Label midiInputLabel;
+
+    void setupMidiDevices();
+    void closeMidiDevices();
+
+    // Flag to check if focus has been set
+    bool hasFocusBeenSet = false;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
